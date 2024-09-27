3eb968d78c3a7283b896acf02efcddbad2e97a0a
pedro092692 <pedro_092692@hotmail.com>
2024-09-27 17:47:13 -0400

    add requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from web import Web
+from scraper import Scraper
+
+topic = 'php'
 
 web = Web()
+web_content = web.get_webpage(topic=topic)
 
+
+scraper = Scraper(web=web_content, topic=topic)
+print(scraper.get_jobs())
--- scraper.py
+++ scraper.py
@@ -3,5 +3,19 @@ from bs4 import BeautifulSoup
 
 class Scraper:
 
-    def __init__(self):
-        pass
+    def __init__(self, web, topic):
+        self.topic = topic
+        self.web_content = web
+        self.soup = BeautifulSoup(self.web_content, "html.parser")
+
+    def get_jobs(self):
+        data = self.soup
+        jobs_section = data.find(name='section', class_='card-list-container')
+        if jobs_section:
+            # find number of pages
+            page_numbers = data.find_all(name='div', class_='sr-only')
+            print(int(page_numbers[-2].text[24::].lstrip()))
+            return jobs_section
+        else:
+            print(f"Sorry not jobs found for: '{self.topic}'")
+            exit(0)
